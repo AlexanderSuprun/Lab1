bda4f4656dc57c87d01be25fec357c698cfb34a1
AlexanderSuprun <42186777+AlexanderSuprun@users.noreply.github.com>
2021-01-09 18:16:01 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println(" \t Информация о студенте");
-	    System.out.println("\n Ф.И.О.: \t Супрун Александр Олегович \n Дата рождения: \t 08.03.1999");
 	    System.out.print(" Университет: \t ОНПУ \n Факультет: \t ИКС \n Курс: \t\t\t 3");
     }
 }
